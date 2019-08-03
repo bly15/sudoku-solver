fa05a03101f846c6801c80f285cf07554cae1dfe
Brian Ly <17816800+bly15@users.noreply.github.com>
2019-08-03 00:39:05 -0700

    Create sudoku table interface

---
+++ solver/static/solver/input.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+    setInputFilter(document.getElementById("intLimit"), function(value) {
+        return /^\d*$/.test(value) && (value === "" || parseInt(value) >= 1 && parseInt(value) <= 9);
+    });
+}
+
+// Restricts input for the given textbox to the given inputFilter
+function setInputFilter(textbox, inputFilter) {
+    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+        textbox.addEventListener(event, function() {
+            if (inputFilter(this.value)) {
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            }
+        });
+    });
+}
--- solver/views.py
+++ solver/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-
 def index(request):
-    return HttpResponse("Welcome to Sudoku Solver!")
+    return render(request, 'solver/index.html')
