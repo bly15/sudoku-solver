d0714bb310cc92935088b429c844a7fc8ec013dc
Brian Ly <17816800+bly15@users.noreply.github.com>
2019-08-04 19:43:01 -0700

    Restrict sudoku table input to be values between 1-9

--- solver/static/solver/input.js
+++ solver/static/solver/input.js
@@ -1,25 +1,10 @@
-window.onload = function() {
-    setInputFilter(document.getElementById("intLimit"), function(value) {
-        return /^\d*$/.test(value) && (value === "" || parseInt(value) >= 1 && parseInt(value) <= 9);
-    });
-}
+$(document).ready(function() {
+    $("[type=tel]").on("change", function(e) {
+        $(e.target).val($(e.target).val().replace(/[^\d\.]/g, ""));
+    })
 
-/**
- * Restricts input for the given textbox to the given inputFilter
- * @param {textbox} textbox HTML input form
- * @param {input} inputFilter User input to HTML input form
- */
-function setInputFilter(textbox, inputFilter) {
-    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
-        textbox.addEventListener(event, function() {
-            if (inputFilter(this.value)) {
-                this.oldValue = this.value;
-                this.oldSelectionStart = this.selectionStart;
-                this.oldSelectionEnd = this.selectionEnd;
-            } else if (this.hasOwnProperty("oldValue")) {
-                this.value = this.oldValue;
-                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-            }
-        });
-    });
-}
+    $("[type=tel]").on("keypress", function(e) {
+        keys = ["1","2","3","4","5","6","7","8","9"];
+        return keys.indexOf(event.key) > -1;
+    })
+});
